b1a81b68bf87c64eb1150d4748dee4b77cf77457
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-02-28 14:08:58 +1100

    Add support for NBVIEWER_LOCALFILES environment variable.

--- start-nbviewer.sh
+++ start-nbviewer.sh
@@ -4,4 +4,8 @@ set -eo pipefail
 
 # Start the nbviewer instance.
 
+if [ ! -z "$NBVIEWER_LOCALFILES" ]; then
+    NBVIEWER_ARGS="$NBVIEWER_ARGS --localfiles=${NBVIEWER_LOCALFILES}"
+fi
+
 exec python -m nbviewer --port=8080  "$@" ${NBVIEWER_ARGS}
